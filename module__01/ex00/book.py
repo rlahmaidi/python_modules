8b1c4f150b05b0f6554caaaaecff962d1678e64e
Rachid Lahmaidi el idrissi <rlahmaid@e2r3p11.1337.ma>
2023-04-04 23:40:43 +0000

    ex01

--- module__01/ex00/book.py
+++ module__01/ex00/book.py
@@ -2,22 +2,23 @@ import sys
 from datetime import datetime
 from datetime import date
 from recipe import Recipe
+import time 
 
 class Book:
-    recipe_lst = []
-    recipes_list = {"starter": [], "lunch": [], "dessert": []}
-    def __init__(self, name, last_update = datetime.now, creation_date = datetime.now()):
+    # recipe_lst = []
+    recipes_list = {"dessert": [], "starter": [], "lunch": []}
+    def __init__(self, name, last_update = datetime.now(), creation_date = datetime.now()):
         if isinstance(name, str) is False:
             print("name of book should be a string")
             sys.exit()
         else:
             self.name = name
-        if isinstance(last_update, datetime.date) is False:
+        if isinstance(last_update, datetime) is False:
             print("last update(3rd arg) should be a datetime")
             sys.exit()
         else:
             self.last_update = last_update
-        if isinstance(creation_date, datetime.date) is False:
+        if isinstance(creation_date, datetime) is False:
             print("creation date(last arg) should be a datetime")
             sys.exit()
         else:
@@ -30,29 +31,60 @@ class Book:
     
     def get_recipe_by_name(self, name):
         """Prints a recipe with the name \texttt{name} and returns the instance"""
-        for i in recipe_lst:
-            if i.name is name:
-                return i
+        for key in self.recipes_list.keys():
+            for i in self.recipes_list[key]:
+                if i is name:
+                    print(i.__str__())
+                    return
+        return
         
     def get_recipes_by_types(self, recipe_type):
         """Get all recipe names for a given recipe_type """
-        lst = []
-        for i in recipe_lst:
-            if i.recipe_type is recipe_type:
-                lst.append(i)
-        return lst
+        types = ["dessert", "lunch", "starter"]
+        if recipe_type  in types:
+            for key in self.recipes_list.keys():
+                if recipe_type is key:
+                    return self.recipes_list[key]
+        else:
+            print("this type dosen't exist in the book")
+            return
     
     def add_recipe(self, recipe):
         """Add a recipe to the book and update last_update"""
         if isinstance(recipe, Recipe) is False:# to be tested
             print("you can only add propper recipes to this book")
-        elif recipe.recipe_type is "desset":
-            recipes_list["dessert"].append(recipe)
+        elif recipe.type == 'desset':
+            print("do we get inside the desired if")
+            self.recipes_list["dessert"].append(recipe)
+        else:
+            self.recipes_list["dessert"].append(recipe)
+        self.last_update = datetime.now()
+        # print("from add printing the dictionary:")
+        # print(self.recipes_list)
     
     def print_dectionary(self):
-        print(recipes_list)
+        print("we are now inside the print_dectionary")
+        print("the name of the book is: ", self.name)
+        print("the creation date is ", self.creation_date)
+        print("the last update is: ",self.last_update)
+        print("the book the following content")
+        for key in self.recipes_list.keys():
+            for i in self.recipes_list[key]:
+                print(i.__str__())
 
 
-if __name__ == "__main__":
-    book = Boo
-    
+# if __name__ == "__main__":
+#     book = Book("first book")
+#     recip = Recipe(
+#     "lghda", 3, 23, ["haja", "haja1", "sdf"],
+#     "chi haja katkal", "dessert")
+
+#     recip1 = Recipe("l3cha", 5, 10, ["kafta, bassla, maticha, zit"],\
+#                      "i'm cooking a ma9la now", "dessert")
+#     # print(dir(recip))
+#     # print(recip.__str__())
+#     book.add_recipe(recip)
+#     book.print_dectionary()
+#     time.sleep(2)
+#     book.add_recipe(recip1)
+#     book.print_dectionary()
--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+import sys
 
+class test:
+    dic = {"1":1, "2":2, "2": 4}
+    def fun(self,):
+        self.dic["1"] = 6
+        print(self.dic)
+
+t = test()
+print(t.dic)
+t.fun()
