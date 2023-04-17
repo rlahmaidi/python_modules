4ae581d4139f1260fb1420af40df2473e711631b
Rachid Lahmaidi el idrissi <rlahmaid@e1r2p1.1337.ma>
2023-04-17 17:12:49 +0000

    dkfjkdfj

--- module__01/ex01/game.py
+++ module__01/ex01/game.py
@@ -1,13 +1,39 @@
-class   GotCharacter:
-    def __init__(self, first_name, is_alive = True):
+class GotCharacter:
+    """This the GotCharacter Class
+        it tells the name of the character
+        and wether it is alive or not"""
+    def __init__(self, first_name, is_alive=True):
         self.first_name = first_name
         if is_alive in (True, False):
             self.is_alive = is_alive
         else:
             print("the is_alive can only be true of false")
 
+
 class Stark(GotCharacter):
+    """A class representing the Stark family. Or when bad
+      things happen to good people."""
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
         self.house_words = "Winter is Coming"
+
+    def print_house_words(self):
+        print(self.house_words)
+
+    def die(self):
+        self.is_alive = False
+
+
+class Lannister(GotCharacter):
+    """Fair-haired, tall and handsome and we win at whatever price"""
+    def __init__(self, first_name=None, is_alive=True):
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = "Lannister"
+        self.house_words = "A Lannister Always Pays His Debts."
+
+    def print_house_words(self):
+        print(self.house_words)
+
+    def die(self):
+        self.is_alive = False
