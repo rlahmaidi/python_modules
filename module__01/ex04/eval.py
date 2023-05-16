8744cc13353956f3c6bde331f702db5171521a60
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-05-16 04:03:01 +0100

    ex04 done

--- module__01/ex04/eval.py
+++ module__01/ex04/eval.py
@@ -2,18 +2,53 @@ import sys
 
 
 class Evaluator:
-    def __init(self,):
+    def __init(self):
         pass
-    def zip_evaluate(self,coefs, words):
-        if not isinstance((coefs, words), list):
+
+    @staticmethod
+    def zip_evaluate(coefs, words):
+        sum = 0
+        if not (isinstance(coefs, list) and isinstance(words, list)):
             print("this function takes two list as a param")
-            sys.exit()
+            return -1
         elif len(coefs) != len(words):
             print("the two list should be the same length")
             return -1
+        elif not all(isinstance(n, (int, float)) for n in coefs):
+            print("the coefecient should all be scalars")
+            return -1
         else:
-            for coef,word in zip(coefs,words):
-                
+            for coef, word in zip(coefs, words):
+                sum += len(word) * coef
+        return(sum)
 
-    
-    def enumerate_evaluate(self,coefs, words):
+    @staticmethod
+    def fun(coefs):
+        for count, coef in enumerate(coefs):
+            yield coef
+
+    @staticmethod
+    def enumerate_evaluate(coefs, words):
+        sum = 0
+        if not (isinstance(coefs, list) and isinstance(words, list)):
+            print("this function takes two list as a param")
+            return -1
+        elif len(coefs) != len(words):
+            print("the two list should be the same length")
+            return -1
+        elif not all(isinstance(n, (float, int)) for n in coefs):
+            print("the coefecient should all be scalars")
+            return -1
+        else:
+            it = Evaluator.fun(coefs)
+            for count, word in enumerate(words):
+                # it = Evaluator.fun(coefs)
+                # the above is not correct because
+                # each time you get an iterator on the begining
+                sum += len(word) * next(it)
+        return sum
+
+# words = ["lsii","lsa"]
+# coefs = [0.5,1]
+# print(Evaluator.zip_evaluate(coefs,words))
+# print(Evaluator.enumerate_evaluate(coefs, words))
--- module__01/ex04/test.py
+++ module__01/ex04/test.py
@@ -3,11 +3,10 @@
 # for count,element in enumerate(lst):
 #     # print(count)
 #     print(element)
-# names = ['Alice', 'Bob', 'Charlie']
-# ages = [24, 50, 18]
+names = ['Alice', 'Bob', 'Charlie']
+ages = [24, 50, 18]
 
-# for  name in zip(names, ages):
-#     print( name)
-lst = ["1","2"]
-sum = sum(lst)
-print(sum)
+for  (name, age) in (enumerate(names),enumerate(ages)):
+    print( name)
+    print(age)
+
