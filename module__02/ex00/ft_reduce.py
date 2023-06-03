694794301f72eefe3525d2b6dabedc6b85b796d4
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-03 06:22:35 +0100

    only exceptions are missed

--- module__02/ex00/ft_reduce.py
+++ module__02/ex00/ft_reduce.py
@@ -14,13 +14,16 @@ def ft_reduce(function_to_apply, iterable):
     elif len(iterable) == 1:
         return iterable[0]
     else:
-        result = function_to_apply(iter)
-    # for element in iterable:
-    #     next(element)
-    #     print(element)
-    #     return function_to_apply()
+        result = iterable[0]
+        for i in range(1,len(iterable)):
+            result = function_to_apply(result,iterable[i])
+        return result
 
 
 if __name__ == "__main__":
     lst = ['H', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']
-    print(reduce(lambda u, v: u + v, lst))
+    intergers = [0,1,2,3]
+    print("theirs: ", reduce(lambda u, v: u + v, lst))
+    print("my own: ", ft_reduce(lambda u, v: u + v, lst))
+    print("theirs: ", reduce(lambda u, v: v * 2, intergers))
+    print("my own: ", ft_reduce(lambda u, v: v * 2, intergers))
--- module__02/ex00/rand_test.py
+++ module__02/ex00/rand_test.py
@@ -28,11 +28,11 @@
 # python code to demonstrate working of reduce()
  
 # importing functools for reduce()
-import functools
+from  functools import reduce
  
 # initializing list
-lis = [1, 3, 5, 6, 2]
+lis = [1]
  
 # using reduce to compute sum of list
 print("The sum of the list elements is : ", end="")
-print(functools.reduce(lambda a,b: a *2, lis))
+print(reduce(lambda a,b: a *b, lis))
