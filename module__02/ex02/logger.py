95d488b2c66beee8737deced6bee01fdcc1ac416
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-07 15:14:22 +0100

    logger only need tests

--- module__02/ex02/logger.py
+++ module__02/ex02/logger.py
@@ -1,8 +1,22 @@
-import time
+from time import time,sleep
 from random import randint
 import os
 
-#... your definition of log decorator...
+def log(fun):
+    def inner(ref, water_level = None):
+        start = time()
+        if water_level != None:
+            fun(ref, water_level)
+        else:
+            fun(ref)
+        end = time()
+        to_print = "(cmaxime)Running: " + fun.__name__.ljust(20,) + "[ exec_time = " + str(end - start).ljust(5) + " ms ]\n"
+        log_file = open(r"machine.log","a")
+        log_file.write(to_print)
+    return inner
+
+        
+# (cmaxime)Running: Start Machine [ exec-time = 0.001 ms ]
 class CoffeeMachine():
     water_level = 100
     @log
@@ -25,13 +39,15 @@ class CoffeeMachine():
             print("Coffee is ready!")
     @log
     def add_water(self, water_level):
-        time.sleep(randint(1, 5))
+        sleep(randint(1, 5))
         self.water_level += water_level
         print("Blub blub blub...")
 
 if __name__ == "__main__":
     machine = CoffeeMachine()
+    # print(machine.start_machine.__name__)
     for i in range(0, 5):
-    machine.make_coffee()
+        print("iteration number ", i)
+        machine.make_coffee()
     machine.make_coffee()
     machine.add_water(70)
--- module__02/ex02/tests.py
+++ module__02/ex02/tests.py
@@ -1,18 +1,21 @@
 
-import time
-import math
+# import time
+# import math
 
-def calculate_time(func):
-	def inner1(*args, **kwargs):
-		begin = time.time()
-		func(*args, **kwargs)
-		end = time.time()
-		print("Total time taken in : ", func.__name__, end - begin)
-	return inner1
+# def calculate_time(func):
+# 	def inner1(*args, **kwargs):
+# 		begin = time.time()
+# 		func(*args, **kwargs)
+# 		end = time.time()
+# 		print("Total time taken in : ", func.__name__, end - begin)
+# 	return inner1
 
-@calculate_time
-def factorial(num):
-	time.sleep(2)
-	print(math.factorial(num))
+# @calculate_time
+# def factorial(num):
+# 	time.sleep(2)
+# 	print(math.factorial(num))
 
-factorial(10)
+# factorial(10)
+
+s = "rachid" + str(10)
+print(s)
