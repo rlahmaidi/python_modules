eb45b674cba56a28ee3d982e8d1f3668eb1954b5
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-09 15:57:43 +0100

    only a touch of art is missing

--- module__02/ex03/csvreader.py
+++ module__02/ex03/csvreader.py
@@ -10,14 +10,17 @@ class CsvReader():
     def __enter__(self,):
         if self.filename == None:
             return None
+        print("the filname is ", self.filename)
         file = open(self.filename,"r")
         self.file = file
-        header = self.getheader()
+        header = line = file.readline().strip().split(self.sep)
         for line in file:
+            line = line.strip()
             lst = line.split(self.sep)
             if len(lst) != len(header):
                 return None
             if '' in lst:
+                print("we have an empty value")
                 return None
         # return self.getdata()
         return file
@@ -45,7 +48,7 @@ class CsvReader():
             if self.skip_bottom != 0 and count >= self.skip_bottom:
                 break
             # line = line.strip().replace(" ","").replace("\"","")
-            line = line.replace('\\n','')
+            line = line.strip()
             inner_lst = line.split(self.sep)
             lst.append(inner_lst)
             count += 1
@@ -58,26 +61,26 @@ class CsvReader():
         list: representing the data (when self.header is True).
         None: (when self.header is False).
         """
-        # if self.header is False:
-        #     print("we are inside header= false")
-        #     return None
-        file = open(self.filename,"r")
-        line = file.readline().strip().replace(" ","").replace("\"","")
-        file.close()
-        # the above line is long and hardcode-> is should change it
-        # i should try opening the file with "with"
-        lst = line.split(self.sep)
-        return lst
+        if self.header is False:
+            return None
+        elif self.header is True:
+            file = open(self.filename,"r")
+            line = file.readline().strip()
+            file.close()
+            # the above line is long and hardcode-> is should change it
+            # i should try opening the file with "with"
+            lst = line.split(self.sep)
+            return lst
 
 if __name__ == "__main__":
-    obj = CsvReader("./good.csv", ',')
-    lst = obj.getheader()
-    print(lst)
-    data = obj.getdata()
-    for line in data:
-        print(line)
-    # with CsvReader('bad.csv') as file:
-    #     if file == None:
-    #         print("File is corrupted")
-    #     else:
-    #         print("File isn't corrupted")
+    # obj = CsvReader("./good.csv", ',',header = True)
+    # lst = obj.getheader()
+    # print(lst)
+    # data = obj.getdata()
+    # for line in data:
+    #     print(line)
+    with CsvReader('good.csv') as file:
+        if file == None:
+            print("File is corrupted")
+        else:
+            print("File isn't corrupted")
