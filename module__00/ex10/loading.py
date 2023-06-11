087754df96189e133c9eeabb7f1b5793c58029e7
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-11 23:20:10 +0100

    the loading bar is loading ...

---
+++ module__00/ex10/loading.py
@@ -0,0 +1,10 @@
+from tqdm import tqdm
+import time
+ 
+ 
+for i in tqdm (range (101),
+               desc="Loading…",
+               ascii=False, ncols=75):
+    time.sleep(0.01)
+     
+print("Complete.")
