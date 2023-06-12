8fcc14f45187576767ccba8e7e5afdea5f37396a
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-12 15:23:44 +0100

    ex00 cleaned and ex01 started

--- module__03/ex00/rand_test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-lst = [[1,2,3],[4,5]]
-arr = np.array(lst)
-print(arr)
---
+++ module__03/ex01/ImageProcessor.py
@@ -0,0 +1,13 @@
+
+
+class ImageProcessor:
+    def load(path):
+        
+
+
+#  load(path): opens the PNG file specified by the path argument and returns an
+# array with the RGB values of the pixels image. It must display a message specifying
+# the dimensions of the image (e.g. 340 x 500).
+# • display(array): takes a numpy array as an argument and displays the corresponding RGB image.
+# You must handle errors, if the file passed as argument does not exist or if it can’t be
+# read as an image, with an appropriate message of your choice.
