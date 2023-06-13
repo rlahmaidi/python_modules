c89296b2afecba6e7bd8df8b9520f05e13156b38
rlahmaidi <rachidlahmaidielidrissi@gmail.com>
2023-06-13 14:51:23 +0100

    testing testing ...

--- module__03/ex01/test.py
+++ module__03/ex01/test.py
@@ -2,5 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 
-img = np.asarray(Image.open('./image.png'))
-print(repr(img))
+# img = Image.open('./image.png')
+# print("the type of the object is", type(img))
+# print("the format and size and mode are")
+# print(img.format, img.size, img.mode)
+try:
+    img = np.asarray(Image.open('./image.png'))
+except FileNotFoundError as fn:
+    print(fn)
+# print(type(img))
+# print(repr(img))
+# print(img)
